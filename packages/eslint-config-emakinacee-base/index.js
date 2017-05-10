df08e51e87965e477967063029d405bc86d15631
Daniel Goerdes <d.goerdes@emakina.at>
2017-05-10 12:09:04 +0200

    add base index which extends all base rule sets

---
+++ packages/eslint-config-emakinacee-base/index.js
@@ -0,0 +1,28 @@
+module.exports = {
+    extends: [
+        './rules/best-practices',
+        './rules/errors',
+        './rules/es6',
+        // './rules/imports',
+        './rules/node',
+        './rules/promise',
+        './rules/standard',
+        './rules/strict',
+        './rules/style',
+        './rules/variables',
+    ].map(require.resolve),
+
+    parserOptions: {
+        ecmaVersion: 2017,
+        ecmaFeatures: {
+            experimentalObjectRestSpread: true,
+        },
+        sourceType: 'module',
+    },
+
+    globals: {
+        'navigator': false,
+        'document': false,
+        'window': false
+    }
+};
