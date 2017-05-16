39caa6e5b04c291cf02c0b76f7907577a142f118
Daniel Goerdes <d.goerdes@emakina.at>
2017-05-16 12:39:40 +0200

    remove base config as dependency of react package

--- packages/eslint-config-emakinacee-react/index.js
+++ packages/eslint-config-emakinacee-react/index.js
@@ -1,8 +1,5 @@
 module.exports = {
     extends: [
-        // TODO: replace with npm module name of base config - add to as dependency in package.json
-        // 'eslint-config-emakinacee-base',
-        '../eslint-config-emakinacee-base',
         './rules/react',
         './rules/react-a11y',
     ].map(require.resolve)
