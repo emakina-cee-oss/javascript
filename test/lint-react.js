b0bcf6515f056c53ec1591cac2336aa969f7695f
Daniel Goerdes <d.goerdes@emakina.at>
2017-05-16 15:28:16 +0200

    add react test code

---
+++ test/lint-react.js
@@ -0,0 +1,11 @@
+class Foo extends Component {
+    render() {
+        return (
+            <div className="c-header">
+                <a href="#"></a>
+            </div>
+        );
+    }
+}
+
+export default Foo;
