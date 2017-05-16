86b76de985170d493b3334090b7cc80c2934d832
Daniel Goerdes <d.goerdes@emakina.at>
2017-05-16 15:24:41 +0200

    add testcases for all eslint plugins used in base package

--- test/lint-base.js
+++ test/lint-base.js
@@ -1,2 +1,8 @@
-let foo = 'bar';
-console.log(foo);
+// Test eslint-plugin-import
+import * as foo from './lint-base-import';
+
+// Test eslint-plugin-promise
+let iShouldBeConst = new Promise((iShouldBeResolve, iShouldBeReject) => {});
+
+// Test eslint-plugin-standard
+const obj = { foo: 'bar'};
